cba7f853b691c393324c98ae5ad8f85298290f59
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-10-11 17:59:34 +0530

    done done

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5000'
+ }));
+
+ app.use(cors({
+    origin: 'http://localhost:5000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+ }));
 
 // connect mongodb 
 const mongoURL ="mongodb+srv://sudharsan6078:CSSe0IXzlDA2xzCj@done.l9ghk.mongodb.net/";
 // CSSe0IXzlDA2xzCj
 mongoose.connect(mongoURL)
 
-const detailSchema = new mongoose.Schema({
+const DetailSchema = new mongoose.Schema({
     name:String,
     dept:String,
     college:String,
 })
 
-const details = mongoose.model('detail', detailSchema);
+const Details = mongoose.model('Detail', DetailSchema);
+
+
+
+app.post('/', async (req, res)=>{
+    const data = new Details(req.body);
+    try{
+        const savedata = await data.save();
+        res.json({savedata, message:"data updated"});
+    }
+    catch(err){
+        res.status(400).json({message: err.message})
+    }
+})
 
 app.get('/', async (req,res)=>{
     try{
-        const data = await details.find();
+        const data = await Details.find();
         res.json({data , message:"data display"})
     }
     catch(err){
         res.status(400).json({message:err.message})
     }
 })
-
-app.post('/', async (req,res)=>{
-    const data = new details(req.body);
-    try{
-        const savedata = await data.save();
-        res.json({savedata, message:"data updated"});
-    }
-    catch(err){
-        res.status(400).json({message:err.message})
-    }
-})
-
-
 
 const PORT = 3000
 app.listen(PORT , ()=>{
