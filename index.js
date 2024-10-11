608b07b0197b5d7e6ef9cb53ccf6167df9a15b7c
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-10-11 21:11:52 +0530

    ddddd

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const app = express();
 const cors = require('cors');
 
 // Middleware to parse incoming JSON
-app.use(express.json());
-const corsOptions = {
-    origin: 'https://techx-eight.vercel.app', // Remove the trailing slash
-    optionsSuccessStatus: 200
-  };
-  app.use(cors(corsOptions));
 
-  app.use(cors({ origin: '*' }));
+// app.use(express.json());
+// const corsOptions = {
+//     origin: 'https://techx-eight.vercel.app',
+//     optionsSuccessStatus: 200
+//   };
+//   app.use(cors(corsOptions));
+
+//   app.use(cors({ origin: '*' }));
 
 
 // MongoDB connection string
 const mongoURI = 'mongodb+srv://sudharsan6078:CSSe0IXzlDA2xzCj@done.l9ghk.mongodb.net/'; // Change this to your MongoDB URI
 mongoose.connect(mongoURI)
 
-// Define a simple route
-
+// Define a simple router
 
 // Define a User Schema and Model
 const UserSchema = new mongoose.Schema({
